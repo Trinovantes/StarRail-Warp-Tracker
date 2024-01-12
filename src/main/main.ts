b52903685bad6e43a24095b5acd9e8fb6ff93389
Stephen Li <stephenli.ca@gmail.com>
2024-01-11 21:43:31 -0500

    Add auto update

--- src/main/main.ts
+++ src/main/main.ts
@@ -4,6 +4,7 @@ import { setupIpc } from './setup/setupIpc'
 import { setupWindow } from './setup/setupWindow'
 import log from 'electron-log'
 import path from 'upath'
+import { autoUpdater } from 'electron-updater'
 
 const mainLogger = log.scope('main')
 const rendererLogger = log.scope('renderer')
@@ -11,6 +12,10 @@ const rendererLogger = log.scope('renderer')
 async function main() {
     mainLogger.info('Starting Main Process')
     mainLogger.info(`Log saved to "${path.resolve(log.transports.file.getFile().path)}"`)
+
+    autoUpdater.logger = mainLogger
+    const updateResult = await autoUpdater.checkForUpdatesAndNotify()
+    mainLogger.info(updateResult)
 
     await setupIpc(mainLogger, rendererLogger)
     setupWindow(mainLogger)
