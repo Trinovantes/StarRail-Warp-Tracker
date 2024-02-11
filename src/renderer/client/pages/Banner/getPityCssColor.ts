5b601584f49b03d184df90b49523c49dd4a68491
Stephen Li <stephenli.ca@gmail.com>
2024-02-10 19:52:00 -0500

    Update pity color based on banner type

--- src/renderer/client/pages/Banner/getPityCssColor.ts
+++ src/renderer/client/pages/Banner/getPityCssColor.ts
@@ -1,3 +1,4 @@
+import { getMaxBannerPity } from '@/common/StarRail'
 import { BannerWarp } from '@/main/ipc/tracker/BannerHistory'
 
 export function getPityCssColor(bannerWarp: BannerWarp) {
@@ -5,9 +6,15 @@ export function getPityCssColor(bannerWarp: BannerWarp) {
         return 'var(--star-4-dark)'
     }
 
-    if (bannerWarp.pity < 50) {
+    const maxPity = getMaxBannerPity(bannerWarp.bannerType)
+    const softPityStart = maxPity - Math.floor(100 / 6) // Soft pity adds 6% per roll until soft pity reaches 100% (i.e. soft pity for 90 max pity starts at 74)
+
+    const goodLuckThreshold = softPityStart - 10
+    const avgLuckThreshold = softPityStart + 10
+
+    if (bannerWarp.pity < goodLuckThreshold) {
         return 'var(--q-positive)'
-    } else if (bannerWarp.pity < 75) {
+    } else if (bannerWarp.pity < avgLuckThreshold) {
         return 'var(--q-warning)'
     } else {
         return 'var(--q-negative)'
