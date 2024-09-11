71ff6efbeaf5230594562355e6992e109db2b71c
Stephen Li <stephenli.ca@gmail.com>
2024-09-10 21:40:19 -0400

    Fix unrecognized icons not showing default icon

--- src/renderer/client/pages/Banner/getItemIcon.ts
+++ src/renderer/client/pages/Banner/getItemIcon.ts
@@ -1,17 +1,53 @@
 import { WarpItemType } from '@/common/StarRail'
 import { BannerWarp } from '@/main/ipc/WarpTracker/parseWarps'
+import defaultIcon from '@/renderer/client/assets/img/default-item.png'
+
+// ----------------------------------------------------------------------------
+// Load Data
+// ----------------------------------------------------------------------------
+
+function getCharacterIcons() {
+    const imgReq = require.context('@/renderer/client/assets/img/game/icon/character/', false, /\.png$/i)
+    const images = new Map<string, string>()
+
+    for (const imageName of imgReq.keys()) {
+        const imgPath = imgReq(imageName) as string
+        const itemId = imageName.replace(/\.\/(?<itemId>\d+)\.png/, '$<itemId>')
+        images.set(itemId, imgPath)
+    }
+
+    return images
+}
+
+function getLightConeIcons() {
+    const imgReq = require.context('@/renderer/client/assets/img/game/icon/light_cone/', false, /\.png$/i)
+    const images = new Map<string, string>()
+
+    for (const imageName of imgReq.keys()) {
+        const imgPath = imgReq(imageName) as string
+        const itemId = imageName.replace(/\.\/(?<itemId>\d+)\.png/, '$<itemId>')
+        images.set(itemId, imgPath)
+    }
+
+    return images
+}
+
+const characterIcons = getCharacterIcons()
+const lightConeIcons = getLightConeIcons()
+
+// ----------------------------------------------------------------------------
+// Exports
+// ----------------------------------------------------------------------------
 
 export function getItemIcon(bannerWarp: BannerWarp): string {
     switch (bannerWarp.itemType) {
         case WarpItemType.Character:
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-require-imports
-            return require(`@/renderer/client/assets/img/game/icon/character/${bannerWarp.itemId}.png`)
+            return characterIcons.get(bannerWarp.itemId) ?? defaultIcon
 
         case WarpItemType.LightCone:
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-require-imports
-            return require(`@/renderer/client/assets/img/game/icon/light_cone/${bannerWarp.itemId}.png`)
+            return lightConeIcons.get(bannerWarp.itemId) ?? defaultIcon
 
         default:
-            return ''
+            return defaultIcon
     }
 }
